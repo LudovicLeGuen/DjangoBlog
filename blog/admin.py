8480ee127796618fec906a01a724c72a59072332
LudovicLeGuen <ludovicleguen@me.com>
2023-04-09 21:06:20 +0000

    deployment

--- blog/admin.py
+++ blog/admin.py
@@ -5,11 +5,12 @@ from django_summernote.admin import SummernoteModelAdmin
 
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
+
     list_display = ('title', 'slug', 'status', 'created_on')
-    search_fields = ('title', 'content')
-    prepopulated_fields = {'slug': ('title',)}
+    search_fields = ['title', 'content']
     list_filter = ('status', 'created_on')
-    summernote_fields = ('content')
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('content',)
 
 
 @admin.register(Comment)
