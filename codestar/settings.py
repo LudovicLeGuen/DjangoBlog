e1076aa7260dce56366706678da21592e765e6a6
LudovicLeGuen <ludovicleguen@me.com>
2023-04-09 14:23:54 +0000

    add post admin mamagememt

--- codestar/settings.py
+++ codestar/settings.py
@@ -43,7 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'cloudinary_storage',
     'django.contrib.staticfiles',
-    "cloudinary",
+    'cloudinary',
+    'django_summernote',
     'blog',
 ]
 
